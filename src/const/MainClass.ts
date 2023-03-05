f4d90e35b76cf3f48562538d46eab59bc531d86e
robert nasewytewa <robert.nasewytewa@gmail.com>
2023-03-05 14:22:16 -0800

    define main classes, divisions, sections

---
+++ src/const/MainClass.ts
@@ -0,0 +1,14 @@
+import { MainClass } from '../types/dewey'
+
+export const MainClasses: MainClass[] = [
+  { code: '000', name: 'Computer science, information and general works' },
+  { code: '100', name: 'Philosophy and psychology' },
+  { code: '200', name: 'Religion' },
+  { code: '300', name: 'Social sciences' },
+  { code: '400', name: 'Language' },
+  { code: '500', name: 'Pure science' },
+  { code: '600', name: 'Technology' },
+  { code: '700', name: 'Arts and recreation' },
+  { code: '800', name: 'Literature' },
+  { code: '900', name: 'History and geography' },
+]
