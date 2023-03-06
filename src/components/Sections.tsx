14882fa88dc2d5e15c723103f625c57d1741a3e5
robert nasewytewa <robert.nasewytewa@gmail.com>
2023-03-06 00:06:44 -0800

    add custom hook to sections

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -1,27 +1,39 @@
-import React, { useMemo, useState } from 'react'
-import { sectionPicker } from '../picker/_picker'
+import React, { useContext, useEffect, useMemo } from 'react'
+import ControlBar from './controls/ControlBar'
 import List from './lists/List'
 import ListLayout from './layout/ListLayout'
-import useCount from '../hooks/useCount'
-import ControlBar from './controls/ControlBar'
+import MainClassFilterContext from '../context/MainClassFilterContext'
+import { Section } from '../types/dewey'
+import { Sections as sections } from '../const/Section'
+import useFilteredEntries from '../hooks/useFilteredEntries'
 
 export default function Sections () {
-  const [count, increment, decrement] = useCount(5)
-  const [stale, setStale] = useState(1)
+  const context = useContext(MainClassFilterContext)
 
-  const sections = useMemo(() => {
-    return sectionPicker(count)
-  }, [count, stale])
+  const activeCodes = useMemo(() => {
+    const activeFilters = Object.entries(context.filters).filter(filter => filter[1])
+    return activeFilters.map(entry => entry[0])
+  }, [context.filters])
 
-  function handleShuffle () {
-    const newStale = stale * -1
-    setStale(newStale)
-  }
+  const validSections = useMemo(() => {
+    return sections.filter(item => {
+      if (activeCodes.length === 0) return true
+
+      const sectionMainClass = item.code[0]
+      return activeCodes.some(code => code.startsWith(sectionMainClass))
+    })
+  }, [activeCodes])
+
+  const [displaySections, setFilters, increment, decrement, shuffle] = useFilteredEntries<Section>(sections, validSections, 5)
+
+  useEffect(() => {
+    setFilters(validSections)
+  }, [validSections])
 
   return (
     <ListLayout header={'Sections'}>
-      <ControlBar handleUp={increment} handleDown={decrement} handleShuffle={handleShuffle}/>
-      <List display={sections}/>
+      <ControlBar handleUp={increment} handleDown={decrement} handleShuffle={shuffle}/>
+      <List display={displaySections}/>
     </ListLayout>
   )
 }
