ce1d05741647c2318e9c2032723b2f40ca0ee08e
robert nasewytewa <robert.nasewytewa@gmail.com>
2023-03-05 21:10:59 -0800

    add count limit to Sections

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { sectionPicker } from '../picker/_picker'
+import List from './lists/List'
 
 export default function Sections () {
-  const sections = sectionPicker(10)
+  const [count, setCount] = useState(5)
+
+  const sections = sectionPicker(count)
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const count = parseInt(event.target.value, 10)
+    setCount(count)
+  }
 
   return (
     <>
-      { sections.map(section => (
-        <div>
-          <div>{ section.code } | { section.name }</div>
-        </div>
-      ))
-      }
+      <h3>Sections</h3>
+      <div><label><input type="number" value={count} onChange={handleChange}/>count</label></div>
+      <List display={sections}/>
     </>
   )
 }
