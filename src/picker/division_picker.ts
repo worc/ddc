48855001aaa8049cf8f228b8e3779994b4cf11cd
robert nasewytewa <robert.nasewytewa@gmail.com>
2023-03-05 20:49:37 -0800

    add a pick until satisfied for divisions

--- src/picker/division_picker.ts
+++ src/picker/division_picker.ts
@@ -1,7 +1,11 @@
 import { Division } from '../types/dewey'
 import { Divisions } from '../const/Division'
-import pickFromGenerator from '../util/pickFromGenerator'
+import pickFromGenerator, { pickUntilSatisfied } from '../util/pickFromGenerator'
 
 export default function divisionPicker(count = 1): Division[] {
   return pickFromGenerator<Division>(Divisions, count)
+}
+
+export function divisionPickUntilSatisfied(count: number, discriminator: (arg0: Division) => boolean): Division[] {
+  return pickUntilSatisfied<Division>(Divisions, count, discriminator)
 }
