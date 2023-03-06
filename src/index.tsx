61dbddaa4c25e3a811147690e6697c1bfbe89f8e
robert nasewytewa <robert.nasewytewa@gmail.com>
2023-03-05 21:12:13 -0800

    do a little styling

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,32 @@ import { createGlobalStyle } from 'styled-components'
 import App from './app'
 
 const GlobalStyle = createGlobalStyle`
-    #app {
-        font-family: "Rokkitt", serif;
-        font-size: 16px;
-    }
-    
-    h1 {
-        font-size: 40px;
-    }
-    
-    h2 {
-        font-size: 32px;
-    }
+  html {
+    font-family: "Rokkitt", serif;
+    font-size: 16px;
+  }
+  
+  html, body {
+    margin: 0
+  }
+
+  h1, h2, h3 {
+    border: 1px solid red;
+    text-align: center;
+    margin: 0;
+  }
+
+  h1 {
+    font-size: 3.0rem;
+  }
+
+  h2 {
+    font-size: 2.5rem;
+  }
+
+  h3 {
+    font-size: 2.0rem;
+  }
 `
 render(
   <React.Fragment>
