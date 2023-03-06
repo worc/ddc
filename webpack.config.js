8daadde68b9918b96da561dc03553c6f4d8ff3a9
robert nasewytewa <robert.nasewytewa@gmail.com>
2023-03-05 18:09:32 -0800

    build to docs directory for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
 
   output: {
     filename: 'bundle.[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
 
   resolve: {
